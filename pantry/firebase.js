cec172e11c1abf9210c8fc97e568153674a3fa43
Paul Martin Enano <paul.m.enano@gmail.com>
2024-07-30 18:47:57 -0400

    added support

--- pantry/firebase.js
+++ pantry/firebase.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC6BCv_YdUe9V9ldYMrkIGRUzKiRWp5mGA",
-  authDomain: "hspantryapp-ac1fd.firebaseapp.com",
-  projectId: "hspantryapp-ac1fd",
-  storageBucket: "hspantryapp-ac1fd.appspot.com",
-  messagingSenderId: "608191593769",
-  appId: "1:608191593769:web:3b18efceda4cf565748b38"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const firestore = getFirestore(app)
-export {app, firestore}
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export { app, firestore };
